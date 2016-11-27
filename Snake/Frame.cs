e88efd664dab3874c094d47f4139f0e9313fdfe5
KungPaoChicken <c2.lam@student.vu.nl>
2016-11-27 23:09:47 +0100

    Fixed head collision detection bug

--- Snake/Frame.cs
+++ Snake/Frame.cs
@@ -10,7 +10,7 @@ namespace Snake {
         public List<Command> nextFrame(Direction direction) {
             List<Command> commands = new List<Command>();
             Coordinate next_head = nextHead(direction);
-            if (snake.contains(next_head)) {    // have to remove old head first, else the imaginary new head would trigger this
+            if (snake.contains(next_head) && !snake.get(snake.length -1).Equals(next_head)) {
                 commands.Add(new PauseCommand());
             } else {
                 if (snake.get(0).Equals(apple)) {
